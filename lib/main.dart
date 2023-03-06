ed562f16c13812c895051a18a50e77734c6e156a
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2023-03-06 01:32:37 +0100

    create a provider for models

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:chat_gpt/constants/constants.dart';
+import 'package:chat_gpt/providers/models_provider.dart';
 import 'package:chat_gpt/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,16 +13,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chat GPT',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: scaffoldBackgroundColor,
-        appBarTheme: AppBarTheme(
-          color: cardColor
-        )
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ModelsProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Chat GPT',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: scaffoldBackgroundColor,
+          appBarTheme: AppBarTheme(
+            color: cardColor
+          )
+        ),
+        home: ChatScreen(),
       ),
-      home: ChatScreen(),
     );
   }
 }
